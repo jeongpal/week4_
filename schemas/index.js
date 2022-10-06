9169599b3bf177a43838d5152d6eea45d0c084b5
jeongpal <108498421+jeongpal@users.noreply.github.com>
2022-10-06 23:13:16 +0900

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -1,15 +1,3 @@
-// const mongoose = require("mongoose");
-
-// const connect = () => {
-//     mongoose
-//       .connect("mongodb://54.180.144.125:27017/PRIVATE_PROJ")
-//       .catch(err => console.log(err));
-//   };
-  
-  
-//   mongoose.connection.on("error", err => {
-//     console.error("몽고디비 연결 에러", err);
-//   });
 require("dotenv").config();
   const mongoose = require("mongoose");
   // require("dotenv").config();
